f7088be8ffb1aa863b1bb8d1507b5f3f1fe13893
highkh <highkh@estsoft.com>
2017-06-08 18:32:33 +0900

    web page ui 개선

--- webtest/async_test.py
+++ webtest/async_test.py
@@ -27,30 +27,32 @@ class FileHandler(tornado.web.RequestHandler):
             self.u_name = self.get_arguments('urlarg')[0]
             http_client = tornado.httpclient.AsyncHTTPClient()
             http_request = tornado.httpclient.HTTPRequest(url=self.u_name, request_timeout=50.0, connect_timeout=50.0)
-            print 'Test : wait................'
+            print 'url Test : wait................'
             http_client.fetch(http_request, self.handle_response)
-            print 'Test : after fetch~~~~~~~~~~~~~~~~~~~'
+            print 'url Test : after fetch~~~~~~~~~~~~~~~~~~~'
 
         else:
             f_info = self.request.files['filearg'][0]
             u_name = "data:image/jpg;base64," + base64.b64encode(BytesIO(f_info['body']).getvalue())
             img = Image.open(StringIO(f_info['body']))
             top5_res = cls.get_info(img)
+            print 'File Test : request and classification success!!'
             self.render('result_page.html', file_name=u_name,
                         cls_results='Top5 Classification Results',
                         items=top5_res)
+            print 'File Test : Render complete!!!'
 
     def handle_response(self, response):
         if response.error:
             print 'url error : %s' % response.error
         elif response.code == 200:
-            print 'Test : response success!!!!!!!!!!!!!!!!!!!'
+            print 'url Test : response success!!!!!!!!!!!!!!!!!!!'
             img = Image.open(StringIO(response.body))
             top5_res = cls.get_info(img)
             self.render('result_page.html', file_name=self.u_name,
                         cls_results='Top5 Classification Results',
                         items=top5_res)
-            print 'Test : Render process complete!!!'
+            print 'url Test : Render process complete!!!'
 
 
 
